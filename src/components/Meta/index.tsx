ec148b902837e3af299b81045f038a1c8f99cc3b
JexLau <jexlau.dev@gmail.com>
2024-01-12 18:47:45 +0800

    fix: outgoing hreflang

--- src/components/Meta/index.tsx
+++ src/components/Meta/index.tsx
@@ -10,23 +10,21 @@ export interface MetaProps {
 
 
 export const Meta = ({ title, description, locale = 'en', pageurl, image = "https://mlbtlwqqirbfngxpwtno.supabase.co/storage/v1/object/public/mapleadscraper/web/images/export-your-results.png" }: MetaProps) => {
-  // const url = pageurl ? `${baseurl}/${pageurl}` : baseurl
-  // `${baseurl}${pageurl}` : `${baseurl}/${locale}${pageurl}`}
-  const url = pageurl ? `${getHrefLang(locale)}${pageurl}` : getHrefLang(locale)
-
+  const getUrl = (lang: string) => pageurl ? `${getHrefLang(lang)}${pageurl}` : getHrefLang(lang)
+  const url = getUrl(locale);
   return <>
     <title>{title}</title>
     <meta name="description" content={description} />
     <link rel="canonical" href={url} />
-    <link rel="alternate" hrefLang="x-default" href={getHrefLang('en')} />
-    <link rel="alternate" hrefLang="en" href={getHrefLang('en')} />
-    <link rel="alternate" hrefLang="ja" href={getHrefLang('ja')} />
-    <link rel="alternate" hrefLang="de" href={getHrefLang('de')} />
-    <link rel="alternate" hrefLang="ko" href={getHrefLang('ko')} />
-    <link rel="alternate" hrefLang="es" href={getHrefLang('es')} />
-    <link rel="alternate" hrefLang="pt" href={getHrefLang('pt')} />
-    <link rel="alternate" hrefLang="vi" href={getHrefLang('vi')} />
-    <link rel="alternate" hrefLang="fr" href={getHrefLang('fr')} />
+    <link rel="alternate" hrefLang="x-default" href={getUrl('en')} />
+    <link rel="alternate" hrefLang="en" href={getUrl('en')} />
+    <link rel="alternate" hrefLang="ja" href={getUrl('ja')} />
+    <link rel="alternate" hrefLang="de" href={getUrl('de')} />
+    <link rel="alternate" hrefLang="ko" href={getUrl('ko')} />
+    <link rel="alternate" hrefLang="es" href={getUrl('es')} />
+    <link rel="alternate" hrefLang="pt" href={getUrl('pt')} />
+    <link rel="alternate" hrefLang="vi" href={getUrl('vi')} />
+    <link rel="alternate" hrefLang="fr" href={getUrl('fr')} />
     <meta property="og:title" content={title} />
     <meta property="og:type" content="website" />
     <meta property="og:site_name" content={"FreeMapsScraper"} />
